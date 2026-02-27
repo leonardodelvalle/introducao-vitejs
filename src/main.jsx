5e44f44e7963dd5adce3642d25ea4a5eb1503f5a
leonardodelvalle <leonardodelvallegab@gmail.com>
2026-02-27 14:39:22 -0300

    aula 2702

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './reset.css' 
-import './global.css'
+import './css/reset.css' 
+import './css/global.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
---
+++ src/sobre/page.jsx
@@ -0,0 +1,11 @@
+
+function sobre () {
+    return(
+        <div>
+            <h1>sobre</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque atque, eligendi aliquid ad est repellat qui ipsa delectus, necessitatibus natus unde quos consequuntur fuga numquam praesentium quaerat explicabo blanditiis repellendus?</p>
+        </div>
+    )
+}
+
+export default sobre;
