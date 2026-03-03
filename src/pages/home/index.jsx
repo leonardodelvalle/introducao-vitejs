e1dfa4916ed5bbf0766a6fd96b1db8e93b93d465
leonardodelvalle <leonardodelvallegab@gmail.com>
2026-03-03 11:45:11 -0300

    aula 3-3

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div>
+            <h1>sobre</h1>
+            <h3>Estilização CSS</h3>
+            <p>Texto do meu sobre</p>
+        </div>
+    )
+}
+
+export default Home;
