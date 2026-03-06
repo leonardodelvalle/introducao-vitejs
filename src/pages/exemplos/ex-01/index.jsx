382ae5237f06ec722e61bdf8a462b5bd006089ec
leonardodelvalle <leonardodelvallegab@gmail.com>
2026-03-06 14:42:31 -0300

    aula 6-3

---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,10 @@
+function exemplo01 () {
+    return(
+        <div>
+            <h1>exemplo 01</h1>
+            <p>Conteudo minimo para componente react</p>
+        </div>
+    )
+}
+
+export default exemplo01;
---
+++ src/pages/exemplos/ex-02/index.jsx
@@ -0,0 +1,17 @@
+import styles from './index.module.css';
+
+
+function exemplo02() {
+    return (
+        <div className={styles.containerEx2}>
+            <h1>sobre</h1>
+            <h2>Estilização CSS</h2>
+            <h3>Uso de Css em diferentes componentes</h3>
+            <p>Eeu não fiz nada</p>
+            <label>Exemplo Label</label>
+            <span>Exemplo Span</span>
+        </div>
+    )
+}
+
+export default exemplo02;
